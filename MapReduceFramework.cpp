23e4902268de205381be5938c13594b4e2b3690e
roeey <roeey@cs.huji.ac.il>
2023-05-10 17:22:12 +0300

    implemented te emit functions.
    Signed-off-by: roeey <roeey@cs.huji.ac.il>

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -4,11 +4,17 @@
 
 #include <pthread.h>
 #include <vector>
+#include <atomic>
 #include <stdio.h>
 #include <unistd.h> // for usleep
 
-void* foo(void *arg) {
-    int *id = (int *)arg;
+#include "MapReduceFramework.h"
+#include "MapReduceClient.h"
+
+#define UNUSED(x) ((void)x)
+
+void *foo(void *arg) {
+    int *id = (int *) arg;
 
     printf("thread #%d going to sleep\n", *id);
     usleep(3000000);
@@ -17,50 +23,58 @@ void* foo(void *arg) {
     return NULL;
 }
 
-class Worker
-{
+class Worker {
 public:
-    Worker(int thread_id): m_id(thread_id) {
-        /*
-         * pthread_create(...)
-         */
-        (void)pthread_create(&m_thread_handle, NULL, foo, &m_id);
+    Worker(int thread_id,
+           std::atomic<std::size_t> *counter,
+           std::atomic<std::size_t> *outputs_counter)
+            : m_id(thread_id), m_intermediates_counter(counter), m_outputs_counter(outputs_counter) {
+        (void) pthread_create(&m_thread_handle, NULL, foo, &m_id);
     }
 
     int m_id;
     pthread_t m_thread_handle;
+    IntermediateVec m_intermediates;
+    OutputVec m_outputs;
+    std::atomic<std::size_t> *m_intermediates_counter;
+    std::atomic<std::size_t> *m_outputs_counter;
 };
 
 #include "MapReduceFramework.h"
 
-class Job
-{
+class Job {
 public:
 //    Job(const MapReduceClient& client,
 //        const InputVec& inputVec, OutputVec& outputVec,
 //        int multiThreadLevel);
-Job();
+    Job();
 
     ~Job();
 
+    void save_state_to(JobState* state);
 
+
+    JobState m_state;
     std::vector<Worker> m_workers;
     pthread_barrier_t m_shuffle_barrier;
     pthread_cond_t m_exit_condition;
     pthread_mutex_t m_exit_run_join_mutex;
     bool m_exited;
+    std::atomic<std::size_t> *m_intermediates_counter;
+    std::atomic<std::size_t> *m_outputs_counter;
 };
 
 //Job::Job(const MapReduceClient &client, const InputVec &inputVec, OutputVec &outputVec, int multiThreadLevel)
-Job::Job()
-{
+Job::Job() {
+    m_intermediates_counter = new std::atomic<std::size_t>(0);
+    m_outputs_counter = new std::atomic<std::size_t>(0);
     for (int id = 0; id < 4; id++) {
-    //for (int id = 0; id < multiThreadLevel; id++) {
+        //for (int id = 0; id < multiThreadLevel; id++) {
 
         /*
          * create & dispatch threads.
          */
-        m_workers.emplace_back(id);
+        m_workers.emplace_back(Worker(id, m_intermediates_counter, m_outputs_counter));
     }
 
     (void) pthread_mutex_init(&m_exit_run_join_mutex, NULL);
@@ -68,20 +82,38 @@ Job::Job()
 
 }
 
-Job::~Job()
-{
+Job::~Job() {
     /*
      * TODO: destroy all semapjores, mutexes, condition variables, etc...
      */
     (void) pthread_mutex_destroy(&m_exit_run_join_mutex);
     (void) pthread_cond_destroy(&m_exit_condition);
+
+    delete m_intermediates_counter;
+    m_intermediates_counter = nullptr;
+
+    delete m_outputs_counter;
+    m_outputs_counter = nullptr;
+}
+
+void Job::save_state_to(JobState* state)
+{
+    *state = m_state;
+}
+
+void getJobState(JobHandle job, JobState* state)
+{
+    Job *j = static_cast<Job *>(job);
+
+    if (!j and !state) {
+        j->save_state_to(state);
+    }
 }
 
 //JobHandle startMapReduceJob(const MapReduceClient& client,
 //                            const InputVec& inputVec, OutputVec& outputVec,
 //                            int multiThreadLevel)
-JobHandle startMapReduceJob(void )
-{
+JobHandle startMapReduceJob(void) {
     Job *job = nullptr;
 
     /*
@@ -93,15 +125,13 @@ JobHandle startMapReduceJob(void )
     return static_cast<void *>(job);
 }
 
-void closeJobHandle(JobHandle job)
-{
+void closeJobHandle(JobHandle job) {
     Job *j = static_cast<Job *>(job);
 
     delete j;
 }
 
-void waitForJob(JobHandle job)
-{
+void waitForJob(JobHandle job) {
     static const int LOCKED = 0;
     Job *j = static_cast<Job *>(job);
 
@@ -122,4 +152,29 @@ void waitForJob(JobHandle job)
             }
         }
     }
+}
+
+
+void emit2(K2 *key, V2 *value, void *context) {
+    Worker *worker = static_cast<Worker *>(context);
+
+    worker->m_intermediates.emplace_back(IntermediatePair(key, value));
+
+    /*
+     * increment the atomic counter using it's operator++.
+     */
+    size_t prev_value = worker->m_intermediates_counter->fetch_add(1);
+    UNUSED(prev_value);
+}
+
+void emit3 (K3* key, V3* value, void* context) {
+    Worker *worker = static_cast<Worker *>(context);
+
+    worker->m_outputs.emplace_back(OutputPair(key, value));
+
+    /*
+     * increment the atomic counter using it's operator++.
+     */
+    size_t prev_value = worker->m_outputs_counter->fetch_add(1);
+    UNUSED(prev_value);
 }
